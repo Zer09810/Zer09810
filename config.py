dea5ec7c6d6fb158d7e7897b6ca52db4138f5e20
Zer09810 <100539813+Zer09810@users.noreply.github.com>
2022-03-04 22:29:43 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+BOT_TOKEN = '' #token de tu bot
+API_ID = 7684605 # lo creas en https://my.telegram.org/
+API_HASH = ''  # lo creas en https://my.telegram.org/
+OWNER = [5004180573]
+CHAT_ID = '-1001666279733'
+
+# DATOS DEL MOODLE
+USUARIO = ''
+MOODLE_PASSWORD = ''
+USUARIO_ID = 32764 #Tu id
+ZIP_MB = 249
+MOODLE_URL = 'https://evea.uh.cu'
+
+# CUENTA DE MEGA - este bot sirve par mega tambien
+GMAIL_MEGA = 'AQUI TU GMAIL' 
+PASS_MEGA = 'AQUI TU PASS'
